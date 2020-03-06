b0540951c911ab8c258bec9fc618d242001152b2
ia.novozhilov <ia.novozhilov@tensor.ru>
2020-03-06 16:51:21 +0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# core
+# @notml/core [![npm][npmbadge]][npm] [![build][badge]][actions]
+
 Not a HTML - is object-oriented modeling of HTML
+
+[npmbadge]: https://img.shields.io/npm/v/@notml/core
+
+[npm]: https://www.npmjs.com/package/@notml/core
+
+[badge]: https://github.com/@notml/core/workflows/Checks%20%26%20Publish/badge.svg
+
+[actions]: https://github.com/@notml/core/actions
---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import alias from '@rollup/plugin-alias'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import babel from 'rollup-plugin-babel'
+import cleanup from 'rollup-plugin-cleanup'
+import { terser } from 'rollup-plugin-terser'
+
+export default [{
+  input: 'src/external.js',
+  output: { file: 'build/external.js', format: 'esm', compact: true },
+  plugins: [
+    resolve({ browser: true, preferBuiltins: false }),
+    commonjs({ include: 'node_modules/**' }),
+    cleanup({ comments: 'none' }),
+    terser()
+  ]
+}, {
+  input: 'src/qosource.js',
+  external: ['./pako.js'],
+  output: { file: 'build/tmp/qosource.js', format: 'esm' },
+  plugins: [babel({ plugins: ['@babel/plugin-proposal-class-properties'] })]
+}, {
+  input: 'build/tmp/qosource.js',
+  output: { file: 'build/qosource.js', format: 'esm' },
+  plugins: [cleanup({ comments: 'none' })]
+}, {
+  input: 'src/api.js',
+  output: { file: 'api.js', format: 'esm' },
+  plugins: [alias({
+    entries: [
+      { find: './external.js', replacement: '../build/external.js' },
+      { find: './qosource.js', replacement: '../build/qosource.js' }
+    ]
+  })]
+}]
