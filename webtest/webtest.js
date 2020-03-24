890e7005ca7a760d6b37f0d63d080df16ac253a0
IgorNovozhilov <maildrakon@gmail.com>
2020-03-24 22:46:47 +0300

    webtest

---
+++ webtest/webtest.js
@@ -0,0 +1,11 @@
+import { oom } from './notml.js'
+
+
+const div1 = oom('div')
+  .div({ class: 'header' })
+  .div({ class: 'test' }, oom
+    .span('Name: ', { class: 'test-label' })
+    .span('Test', { class: 'test-name' }))
+  .div({ class: 'footer' })
+
+document.getElementById('exp1').append(div1.dom)
