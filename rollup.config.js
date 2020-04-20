e6399ad3469161f0d9bec796724b9799103c32b8
IgorNovozhilov <maildrakon@gmail.com>
2020-04-20 23:59:57 +0300

    up

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,13 @@
 import resolve from '@rollup/plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
 import cleanup from 'rollup-plugin-cleanup'
-import { terser } from 'rollup-plugin-terser'
 
 export default [{
   input: 'src/notml.js',
-  output: { file: 'notml.js', format: 'esm' },
+  output: { file: 'notml.js', format: 'esm', compact: true },
   plugins: [
     resolve({ browser: true, preferBuiltins: false }),
     babel({ plugins: ['@babel/plugin-proposal-class-properties'] }),
     cleanup({ comments: 'none' })
   ]
-}, {
-  input: 'notml.js',
-  output: { file: 'notml.min.js', format: 'esm', compact: true },
-  plugins: [terser()]
 }]
