c49c2504b87b837206cd55f1a46bc94e661bb130
Igor Novozhilov <maildrakon@gmail.com>
2021-07-28 14:12:41 +0300

    +

--- test/3.basic-api.js
+++ test/3.basic-api.js
@@ -1,14 +1,33 @@
 import { assert, Test } from '@nodutilus/test'
 import { oom } from '@notml/core'
 
+const { document } = window
+
+
 /** Проверка API OOM элементов */
 export default class BasicAPI extends Test {
+
+  /**
+   * Добавление дочернего элемента.
+   * Выполняет универсальное добавление элемента с учетом реализации OOM элементов.
+   * Добавление выполняется в конец списка детей, другое поведение нерационально
+   */
+  ['OOMElement#append - Вставка дочернего элемента']() {
+    const div = oom('div')
+
+    div.append(oom('a'))
+    div.append(document.createElement('b'))
+    div.append('test')
+
+    assert.equal(div.html, '<div><a></a><b></b>test</div>')
+  }
+
 
   /**
    * Клонирование элемента для его переиспользования.
    * Создает копию DOM элемента и оборачивает его в новый экземпляр OOM Proxy
    */
-  ['Клонирование через OOMElement#clone']() {
+  ['OOMElement#clone - Клонирование элемента']() {
     const div1 = oom('div', { class: 'test' })
     const div2 = div1.clone()
 
