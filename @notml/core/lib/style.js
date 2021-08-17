513f95183d258c1ff23db42e7ba084c3930027ec
IgorNovozhilov <maildrakon@gmail.com>
2021-08-17 23:46:43 +0300

    +

--- @notml/core/lib/style.js
+++ @notml/core/lib/style.js
@@ -3,11 +3,19 @@ const { customElements, HTMLStyleElement } = window
 
 class OOMStyle extends HTMLStyleElement {
 
+  static updateStyle(target, source) {
+  }
+
+  #style = {}
+
   update(media, ...styles) {
     if (typeof media === 'string') {
       this.setAttribute('media', media)
     } else {
       styles.unshift(media)
+    }
+    for (const style of styles) {
+      OOMStyle.updateStyle(this.#style, style)
     }
   }
 
