e345894e9632fd712c11fd51c4541365c8d4ac92
IgorNovozhilov <maildrakon@gmail.com>
2021-08-24 00:14:12 +0300

    test

--- test/pre-test.js
+++ test/pre-test.js
@@ -4,3 +4,8 @@ mkdirSync('test/node_modules', { recursive: true })
 if (!existsSync('test/node_modules/@notml')) {
   symlinkSync('../../@notml', 'test/node_modules/@notml', 'dir')
 }
+
+mkdirSync('webtest/node_modules', { recursive: true })
+if (!existsSync('webtest/node_modules/@notml')) {
+  symlinkSync('../../@notml', 'webtest/node_modules/@notml', 'dir')
+}
