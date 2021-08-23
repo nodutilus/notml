e23c4c7174120a88b332a2a0e80716f6bc0c77bc
Igor Novozhilov <maildrakon@gmail.com>
2021-08-23 16:17:08 +0300

    test

--- @notml/notml/rollup.config.js
+++ @notml/notml/rollup.config.js
@@ -1,5 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve'
 import cleanup from 'rollup-plugin-cleanup'
+import { terser } from 'rollup-plugin-terser'
+
 
 export default [{
   input: 'src/core.js',
@@ -14,5 +16,20 @@ export default [{
   plugins: [
     resolve({ browser: true, preferBuiltins: false }),
     cleanup({ comments: 'none' })
+  ]
+}, {
+  input: 'core.js',
+  output: { file: 'core.min.js', format: 'esm', compact: true },
+  plugins: [
+    resolve({ browser: true, preferBuiltins: false }),
+    cleanup({ comments: 'none' }),
+    terser()]
+}, {
+  input: 'src/core-global.js',
+  output: { file: 'core-global.min.js', format: 'iife', name: 'oom', compact: true },
+  plugins: [
+    resolve({ browser: true, preferBuiltins: false }),
+    cleanup({ comments: 'none' }),
+    terser()
   ]
 }]
